5199dc36be1437187367ee9411be5fe991fbb952
Валентин <60097296+YurchenkoValentin@users.noreply.github.com>
2020-05-21 20:04:16 +0300

    Подготавливает проект к отправке

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,23 +1,33 @@
-var nav = document.querySelector('.main-nav');
-var close = document.querySelector('.main-nav__close-icon');
-var open = document.querySelector('.main-nav__burger-icon');
 
-close.onclick = function() {
-  nav.classList.toggle('active');
-};
+  var submit = document.querySelector('.form__submit');
+  var popup = document.querySelector('.popup__success');
+  var popuperror = document.querySelector('.popup__error');
+  var close = document.querySelector ('.popup__error-button');
+  var close_s = document.querySelector ('.popup__success-button');
 
-open.onclick = function() {
-  nav.classList.toggle('active');
-};
+  var last_name = document.querySelector("[name=last-name]");
+  var first_name = document.querySelector("[name=first-name]");
+  var email = document.querySelector("[name=email-address]");
 
-var submit = document.querySelector('.form__submit');
-var popup = document.querySelector('.popup');
-var close = document.querySelector ('.popup__button');
+  document.addEventListener("submit", function (evt) {
+    if (!last_name.value || !first_name.value || !email.value) {
+      evt.preventDefault();
+      popuperror.classList.add("show-e");
+    }
+  });
 
-submit.onclick = function() {
-  popup.classList.toggle('active');
-};
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popuperror.classList.remove("show-e");
+  });
 
-close.onclick = function() {
-  popup.classList.toggle('active');
-};
+  document.addEventListener("submit", function (evt) {
+      evt.preventDefault();
+      popup.classList.add("show");
+  });
+
+  close_s.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("show");
+  });
+
